3801727f40b86df6184cdb897dc582583c0e28a6
thanhtungf123 <thanhtungphankt@gmail.com>
2025-10-10 13:43:29 +0700

    auth

--- fe_hotel/src/App.jsx
+++ fe_hotel/src/App.jsx
@@ -5,6 +5,8 @@ import Footer from './layout/Footer'
 import Home from './pages/Home'
 import Search from './pages/Search'
 import RoomDetail from './pages/RoomDetail'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
 export default function App(){
   return (
@@ -14,7 +16,11 @@ export default function App(){
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/rooms/:id" element={<RoomDetail />} /> 
-      </Routes>
+
+       {/* Auth */}
+       <Route path="/login" element={<Login />} />
+       <Route path="/register" element={<Register />} />
+        </Routes>
       <Footer />
     </div>
   )
