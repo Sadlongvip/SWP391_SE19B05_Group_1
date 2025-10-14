f91b77cede90466564416fa6412ab203abe2649f
Sadlongvip <sadlongvip@gmail.com>
2025-10-14 14:45:32 +0700

    .

--- be_hotel/src/main/java/com/luxestay/hotel/model/Employee.java
+++ be_hotel/src/main/java/com/luxestay/hotel/model/Employee.java
@@ -1,12 +1,18 @@
 package com.luxestay.hotel.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,74 +29,66 @@ public class Employee {
     private int status = 1; // 1: active, 0: inactive
     //======================================================
 
-    public Employee() {
-    }
 
     //======================================================
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public LocalDate getHireDate() {
-        return hireDate;
-    }
-
-    public void setHireDate(LocalDate hireDate) {
-        this.hireDate = hireDate;
-    }
-
-    public BigDecimal getSalary() {
-        return salary;
-    }
-
-    public void setSalary(BigDecimal salary) {
-        this.salary = salary;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmployeeGmail() {
-        return employeeGmail;
-    }
-
-    public void setEmployeeGmail(String employeeGmail) {
-        this.employeeGmail = employeeGmail;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//
+//
+//    public String getPosition() {
+//        return position;
+//    }
+//
+//    public void setPosition(String position) {
+//        this.position = position;
+//    }
+//
+//    public String getDepartment() {
+//        return department;
+//    }
+//
+//    public void setDepartment(String department) {
+//        this.department = department;
+//    }
+//
+//    public LocalDate getHireDate() {
+//        return hireDate;
+//    }
+//
+//    public void setHireDate(LocalDate hireDate) {
+//        this.hireDate = hireDate;
+//    }
+//
+//    public BigDecimal getSalary() {
+//        return salary;
+//    }
+//
+//    public void setSalary(BigDecimal salary) {
+//        this.salary = salary;
+//    }
+//
+//    public int getStatus() {
+//        return status;
+//    }
+//
+//    public void setStatus(int status) {
+//        this.status = status;
+//    }
+//
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
